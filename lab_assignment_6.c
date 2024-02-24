434a549920dcd8e07d08dcc38ef02b8e52b0cda6
ycamagay <yn261299@ucf.edu>
2024-02-23 19:32:07 -0500

    assignment completed

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+	if(low > high){
+		return -1;
+	}
+
+	int mid = (high+low)/2;
+
+	if(value < numbers[mid]){
+		return search(numbers, low, mid-1, value);
+	} else if(value > numbers[mid]){
+		return search(numbers, mid+1, high, value);
+	} else {
+		return mid;
+	}
+
 }
 
 void printArray(int numbers[], int sz)
